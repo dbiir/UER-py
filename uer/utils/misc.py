77f5a1a507a4d6ee1152ee6ba9acb1a2618681c5
zhezhaoa <1152543959@qq.com>
2019-10-11 13:26:27 +0800

    change count_lines function

--- uer/utils/misc.py
+++ uer/utils/misc.py
@@ -5,9 +5,12 @@ import torch.nn as nn
 
 def count_lines(file_path):
     lines_num = 0
-    with open(file_path, mode="r", encoding="utf-8") as f:
-        for line in f:
-            lines_num += 1
+    with open(file_path, 'rb') as f:
+        while True:
+            data = f.read(2^20)
+            if not data:
+                break
+            lines_num += data.count(b'\n')
     return lines_num
 
 
