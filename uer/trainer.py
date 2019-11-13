4851eb217379fa420efe851cb35206397d89d4d0
Zhaoxin <38198098+ZhaoxinRuc@users.noreply.github.com>
2019-11-13 16:44:40 +0800

    Update trainer.py

--- uer/trainer.py
+++ uer/trainer.py
@@ -87,7 +87,7 @@ def worker(proc_id, gpu_ranks, args, model):
         {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)], 'weight_decay_rate': 0.0}
     ]
     optimizer = AdamW(optimizer_grouped_parameters, lr=args.learning_rate, correct_bias=False)
-    scheduler = WarmupLinearSchedule(optimizer, warmup_steps=train_steps*args.warmup, t_total=train_steps)
+    scheduler = WarmupLinearSchedule(optimizer, warmup_steps=args.total_steps*args.warmup, t_total=args.total_steps)
 
     if args.dist_train:
         # Initialize multiprocessing distributed training environment.
