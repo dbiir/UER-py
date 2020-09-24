8a4b46f715d2f0c40ec33e94ac3f6dede9c7aeb6
Zhaoxin <38198098+ZhaoxinRuc@users.noreply.github.com>
2020-09-24 12:06:25 +0800

    Update build_vocab.py

--- scripts/build_vocab.py
+++ scripts/build_vocab.py
@@ -38,5 +38,5 @@ if __name__ == '__main__':
 
     # Build and save vocabulary.
     vocab = Vocab()
-    vocab.build(args.corpus, tokenizer, args.workers_num, args.min_count)
+    vocab.build(args.corpus_path, tokenizer, args.workers_num, args.min_count)
     vocab.save(vocab_path)
