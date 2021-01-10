e17b82d2208212324ae012e01170ab9499fbdd8f
Li Yudong <liyudong123@hotmail.com>
2021-01-10 21:42:10 +0800

    Update __init__.py

--- uer/utils/__init__.py
+++ uer/utils/__init__.py
@@ -12,11 +12,11 @@ str2dataset = {"bert": BertDataset, "lm": LmDataset, "mlm": MlmDataset,
 str2dataloader = {"bert": BertDataLoader, "lm": LmDataLoader, "mlm": MlmDataLoader,
                   "bilm": BilmDataLoader, "albert": AlbertDataLoader, "mt": MtDataLoader,
                   "t5": T5DataLoader, "cls": ClsDataLoader}
-str2act = {"gelu": gelu, "gelu_new": gelu_new, "relu": relu}
+str2act = {"gelu": gelu, "gelu_fast": gelu_fast, "relu": relu}
 
 __all__ = ["CharTokenizer", "SpaceTokenizer", "BertTokenizer", "str2tokenizer",
            "BertDataset", "LmDataset", "MlmDataset", "BilmDataset",
            "AlbertDataset", "MtDataset", "T5Dataset", "ClsDataset", "str2dataset",
            "BertDataLoader", "LmDataLoader", "MlmDataLoader", "BilmDataLoader",
            "AlbertDataLoader", "MtDataLoader", "T5DataLoader", "ClsDataLoader", "str2dataloader",
-           "gelu", "gelu_new", "relu", "str2act"]
+           "gelu", "gelu_fast", "relu", "str2act"]
