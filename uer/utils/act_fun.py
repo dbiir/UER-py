455e003cc944997a7f33b04c35c6bd03e20d8ec2
Li Yudong <liyudong123@hotmail.com>
2021-01-10 21:40:22 +0800

    Update act_fun.py

--- uer/utils/act_fun.py
+++ uer/utils/act_fun.py
@@ -6,7 +6,7 @@ import torch.nn.functional as F
 def gelu(x):
     return x * 0.5 * (1.0 + torch.erf(x / math.sqrt(2.0)))
 
-def gelu_new(x):
+def gelu_fast(x):
     return 0.5 * x * (1.0 + torch.tanh(math.sqrt(2.0 / math.pi) * (x + 0.044715 * torch.pow(x, 3.0))))
 
 def relu(x):
