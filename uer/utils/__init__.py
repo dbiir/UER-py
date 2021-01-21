6449a3101d327ab97a96a337965c53442b127ed2
hhou435 <435405393@qq.com>
2021-01-21 11:44:28 +0800

    add silu

--- uer/utils/__init__.py
+++ uer/utils/__init__.py
@@ -12,11 +12,11 @@ str2dataset = {"bert": BertDataset, "lm": LmDataset, "mlm": MlmDataset,
 str2dataloader = {"bert": BertDataLoader, "lm": LmDataLoader, "mlm": MlmDataLoader,
                   "bilm": BilmDataLoader, "albert": AlbertDataLoader, "mt": MtDataLoader,
                   "t5": T5DataLoader, "cls": ClsDataLoader}
-str2act = {"gelu": gelu, "gelu_fast": gelu_fast, "relu": relu}
+str2act = {"gelu": gelu, "gelu_fast": gelu_fast, "relu": relu, "silu": silu}
 
 __all__ = ["CharTokenizer", "SpaceTokenizer", "BertTokenizer", "str2tokenizer",
            "BertDataset", "LmDataset", "MlmDataset", "BilmDataset",
            "AlbertDataset", "MtDataset", "T5Dataset", "ClsDataset", "str2dataset",
            "BertDataLoader", "LmDataLoader", "MlmDataLoader", "BilmDataLoader",
            "AlbertDataLoader", "MtDataLoader", "T5DataLoader", "ClsDataLoader", "str2dataloader",
-           "gelu", "gelu_fast", "relu", "str2act"]
+           "gelu", "gelu_fast", "relu", "silu", "str2act"]
--- uer/utils/act_fun.py
+++ uer/utils/act_fun.py
@@ -1,6 +1,7 @@
 import math
 import torch
 import torch.nn.functional as F
+from packaging import version
 
 
 def gelu(x):
@@ -11,3 +12,19 @@ def gelu_fast(x):
 
 def relu(x):
     return F.relu(x)
+
+def _silu_python(x):
+    """
+    See Gaussian Error Linear Units (Hendrycks et al., https://arxiv.org/abs/1606.08415) where the SiLU (Sigmoid Linear
+    Unit) was originally introduced and coined, and see Sigmoid-Weighted Linear Units for Neural Network Function
+    Approximation in Reinforcement Learning (Elfwing et al., https://arxiv.org/abs/1702.03118) and Swish: a Self-Gated
+    Activation Function (Ramachandran et al., https://arxiv.org/abs/1710.05941v1) where the SiLU was experimented with
+    later.
+    """
+    return x * torch.sigmoid(x)
+
+
+if version.parse(torch.__version__) < version.parse("1.7"):
+    silu = _silu_python
+else:
+    silu = F.silu
