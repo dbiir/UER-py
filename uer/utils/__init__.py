5da2cbbd776ca3c64175dcb35ce1b9b409bcee79
zhezhaoa <1152543959@qq.com>
2021-02-02 20:18:24 +0800

    update optimizers.

--- uer/utils/__init__.py
+++ uer/utils/__init__.py
@@ -35,4 +35,4 @@ __all__ = ["CharTokenizer", "SpaceTokenizer", "BertTokenizer", "str2tokenizer",
            "get_linear_schedule_with_warmup", "get_cosine_schedule_with_warmup",
            "get_cosine_with_hard_restarts_schedule_with_warmup",
            "get_polynomial_decay_schedule_with_warmup",
-           "constant_with_warmup", "get_constant_schedule_with_warmup", "str2scheduler"]
+           "get_constant_schedule", "get_constant_schedule_with_warmup", "str2scheduler"]
