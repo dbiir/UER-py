6a5c9bc15d733513cdb2711d4fdaf5f7423610a6
P01son6415 <liyudong123@hotmail.com>
2021-04-01 11:42:54 +0800

    add run_classifier_grid.py

---
+++ run_classifier_grid.py
@@ -0,0 +1,120 @@
+"""
+This script provides an exmaple to wrap UER-py for classification grid search.
+"""
+import torch
+import random
+import argparse
+from itertools import product
+from run_classifier import *
+
+
+def main():
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    finetune_opts(parser)
+
+    parser.add_argument("--pooling", choices=["mean", "max", "first", "last"], default="first",
+                        help="Pooling type.")
+
+    parser.add_argument("--tokenizer", choices=["bert", "char", "space"], default="bert",
+                        help="Specify the tokenizer."
+                             "Original Google BERT uses bert tokenizer on Chinese corpus."
+                             "Char tokenizer segments sentences into characters."
+                             "Space tokenizer segments sentences into words according to space."
+                        )
+
+    parser.add_argument("--soft_targets", action='store_true',
+                        help="Train model with logits.")
+    parser.add_argument("--soft_alpha", type=float, default=0.5,
+                        help="Weight of the soft targets loss.")
+
+    parser.add_argument("--batch_size_grid", default=[32], nargs='+', type=int,
+                        help="Batch size.")
+    parser.add_argument("--learning_rate_grid", type=float, default=[2e-5], nargs='+',
+                        help="Learning rate.")
+    parser.add_argument("--epochs_num_grid", type=int, default=[3], nargs='+',
+                        help="Number of epochs.")
+
+    args = parser.parse_args()
+
+    # Load the hyperparameters from the config file.
+    args = load_hyperparam(args)
+
+    #set_seed(args.seed)
+
+    # Count the number of labels.
+    args.labels_num = count_labels_num(args.train_path)
+
+    # Build tokenizer.
+    args.tokenizer = globals()[args.tokenizer.capitalize() + "Tokenizer"](args)
+
+    best_acc = 0
+    result = {}
+
+    for batch_size, learning_rate, epochs_num in product(args.batch_size_grid, args.learning_rate_grid, args.epochs_num_grid):
+
+        args.learning_rate = learning_rate
+        args.batch_size = batch_size
+        args.epochs_num = epochs_num
+
+        # build dataset
+
+        trainset = read_dataset(args, args.train_path)
+
+        random.shuffle(trainset)
+        instances_num = len(trainset)
+        batch_size = args.batch_size
+
+        src = torch.LongTensor([example[0] for example in trainset])
+        tgt = torch.LongTensor([example[1] for example in trainset])
+        seg = torch.LongTensor([example[2] for example in trainset])
+        if args.soft_targets:
+            soft_tgt = torch.FloatTensor([example[3] for example in trainset])
+        else:
+            soft_tgt = None
+
+        args.train_steps = int(instances_num * args.epochs_num / batch_size) + 1
+
+
+        # Build classification model.
+        model = Classifier(args)
+
+        args.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        model = model.to(args.device)
+        load_or_initialize_parameters(args, model)
+        optimizer, scheduler = build_optimizer(args, model)
+        if args.fp16:
+            try:
+                from apex import amp
+            except ImportError:
+                raise ImportError("Please install apex from https://www.github.com/nvidia/apex to use fp16 training.")
+            model, optimizer = amp.initialize(model, optimizer, opt_level = args.fp16_opt_level)
+            args.amp = amp
+        if torch.cuda.device_count() > 1:
+            model = torch.nn.DataParallel(model)
+        args.model = model
+
+
+        # Training phase.
+
+        total_loss, _, _ = 0., 0., 0.
+
+        for _ in range(1, args.epochs_num+1):
+            model.train()
+            for i, (src_batch, tgt_batch, seg_batch, soft_tgt_batch) in enumerate(batch_loader(batch_size, src, tgt, seg, soft_tgt)):
+                loss = train_model(args, model, optimizer, scheduler, src_batch, tgt_batch, seg_batch, soft_tgt_batch)
+                total_loss += loss.item()
+                if (i + 1) % args.report_steps == 0:
+                    total_loss = 0.
+
+        acc, _ = evaluate(args, read_dataset(args, args.dev_path))
+
+        if acc > best_acc:
+            best_acc = acc
+            result = {'learning_rate':learning_rate,'batch_size':batch_size, 'epochs_num':epochs_num}
+        print('on', result, '\n')
+
+    print('Best Acc. is:', best_acc, '. On ', result, 'config.')
+
+if __name__ == "__main__":
+    main()
