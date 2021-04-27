8ea31720faceedefb212f2884ecd488cdd93c7f7
hhou435 <435405393@qq.com>
2021-04-27 21:06:54 +0800

    Add convert scripts

---
+++ scripts/convert_bert_token_classification_from_huggingface_to_uer.py
@@ -0,0 +1,32 @@
+import torch
+import argparse
+import collections
+from scripts.convert_bert_from_huggingface_to_uer import convert_bert_transformer_encoder_from_huggingface_to_uer
+
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("--input_model_path", type=str, default="huggingface_model.bin",
+                    help=".")
+parser.add_argument("--output_model_path", type=str, default="pytorch_model.bin",
+                    help=".")
+parser.add_argument("--layers_num", type=int, default=12, help=".")
+
+args = parser.parse_args()
+path = args.input_model_path
+
+input_model = torch.load(args.input_model_path, map_location='cpu')
+
+output_model = collections.OrderedDict()
+
+output_model["embedding.word_embedding.weight"] = input_model["bert.embeddings.word_embeddings.weight"]
+output_model["embedding.position_embedding.weight"] = input_model["bert.embeddings.position_embeddings.weight"]
+output_model["embedding.segment_embedding.weight"] = torch.cat((torch.Tensor([[0]*input_model["bert.embeddings.token_type_embeddings.weight"].size()[1]]), input_model["bert.embeddings.token_type_embeddings.weight"]), dim=0)
+output_model["embedding.layer_norm.gamma"] = input_model["bert.embeddings.LayerNorm.weight"]
+output_model["embedding.layer_norm.beta"] = input_model["bert.embeddings.LayerNorm.bias"]
+
+convert_bert_transformer_encoder_from_huggingface_to_uer(args.layers_num, output_model, input_model)
+
+output_model["output_layer.weight"] = input_model["classifier.weight"]
+output_model["output_layer.bias"] = input_model["classifier.bias"]
+
+
+torch.save(output_model, args.output_model_path)
---
+++ scripts/convert_bert_token_classification_from_uer_to_huggingface.py
@@ -0,0 +1,32 @@
+import torch
+import argparse
+import collections
+from scripts.convert_bert_from_uer_to_huggingface import convert_bert_transformer_encoder_from_uer_to_huggingface
+
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("--input_model_path", type=str, default="pytorch_model.bin",
+                    help=".")
+parser.add_argument("--output_model_path", type=str, default="huggingface_model.bin",
+                    help=".")
+parser.add_argument("--layers_num", type=int, default=12, help=".")
+
+args = parser.parse_args()
+path = args.input_model_path
+
+input_model = torch.load(args.input_model_path)
+
+output_model = collections.OrderedDict()
+
+output_model["bert.embeddings.word_embeddings.weight"] = input_model["embedding.word_embedding.weight"]
+output_model["bert.embeddings.position_embeddings.weight"] = input_model["embedding.position_embedding.weight"]
+output_model["bert.embeddings.token_type_embeddings.weight"] = input_model["embedding.segment_embedding.weight"][1:, :]
+output_model["bert.embeddings.LayerNorm.weight"] = input_model["embedding.layer_norm.gamma"]
+output_model["bert.embeddings.LayerNorm.bias"] = input_model["embedding.layer_norm.beta"]
+
+convert_bert_transformer_encoder_from_uer_to_huggingface(args.layers_num, output_model, input_model)
+
+output_model["classifier.weight"] = input_model["output_layer.weight"]
+output_model["classifier.bias"] = input_model["output_layer.bias"]
+
+
+torch.save(output_model, args.output_model_path)
