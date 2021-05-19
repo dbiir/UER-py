c12031fd8424a51b2930c4cd2e194322ebaf04ad
hhou435 <435405393@qq.com>
2021-05-19 09:41:48 +0800

    Add albert convert scripts

---
+++ scripts/convert_albert_from_huggingface_to_uer.py
@@ -0,0 +1,54 @@
+import torch
+import argparse
+import collections
+
+
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("--input_model_path", type=str, default="huggingface_model.bin",
+                    help=".")
+parser.add_argument("--output_model_path", type=str, default="pytorch_model.bin",
+                    help=".")
+
+args = parser.parse_args()
+
+input_model = torch.load(args.input_model_path)
+
+output_model = collections.OrderedDict()
+
+output_model["embedding.word_embedding.weight"] = input_model["albert.embeddings.word_embeddings.weight"]
+output_model["embedding.position_embedding.weight"] = input_model["albert.embeddings.position_embeddings.weight"]
+output_model["embedding.segment_embedding.weight"] = input_model["albert.embeddings.token_type_embeddings.weight"][1:, :]
+output_model["embedding.layer_norm.gamma"] = input_model["albert.embeddings.LayerNorm.weight"]
+output_model["embedding.layer_norm.beta"] = input_model["albert.embeddings.LayerNorm.bias"]
+
+output_model["encoder.linear.weight"] = input_model["albert.encoder.embedding_hidden_mapping_in.weight"]
+output_model["encoder.linear.bias"] = input_model["albert.encoder.embedding_hidden_mapping_in.bias"]
+output_model["encoder.transformer.layer_norm_2.gamma"] = input_model["albert.encoder.albert_layer_groups.0.albert_layers.0.full_layer_layer_norm.weight"]
+output_model["encoder.transformer.layer_norm_2.beta"] = input_model["albert.encoder.albert_layer_groups.0.albert_layers.0.full_layer_layer_norm.bias"]
+output_model["encoder.transformer.self_attn.linear_layers.0.weight"] = input_model["albert.encoder.albert_layer_groups.0.albert_layers.0.attention.query.weight"]
+output_model["encoder.transformer.self_attn.linear_layers.0.bias"] = input_model["albert.encoder.albert_layer_groups.0.albert_layers.0.attention.query.bias"]
+output_model["encoder.transformer.self_attn.linear_layers.1.weight"] = input_model["albert.encoder.albert_layer_groups.0.albert_layers.0.attention.key.weight"]
+output_model["encoder.transformer.self_attn.linear_layers.1.bias"] = input_model["albert.encoder.albert_layer_groups.0.albert_layers.0.attention.key.bias"]
+output_model["encoder.transformer.self_attn.linear_layers.2.weight"] = input_model["albert.encoder.albert_layer_groups.0.albert_layers.0.attention.value.weight"]
+output_model["encoder.transformer.self_attn.linear_layers.2.bias"] = input_model["albert.encoder.albert_layer_groups.0.albert_layers.0.attention.value.bias"]
+output_model["encoder.transformer.self_attn.final_linear.weight"] = input_model["albert.encoder.albert_layer_groups.0.albert_layers.0.attention.dense.weight"]
+output_model["encoder.transformer.self_attn.final_linear.bias"] = input_model["albert.encoder.albert_layer_groups.0.albert_layers.0.attention.dense.bias"]
+output_model["encoder.transformer.layer_norm_1.gamma"] = input_model["albert.encoder.albert_layer_groups.0.albert_layers.0.attention.LayerNorm.weight"]
+output_model["encoder.transformer.layer_norm_1.beta"] = input_model["albert.encoder.albert_layer_groups.0.albert_layers.0.attention.LayerNorm.bias"]
+output_model["encoder.transformer.feed_forward.linear_1.weight"] = input_model["albert.encoder.albert_layer_groups.0.albert_layers.0.ffn.weight"]
+output_model["encoder.transformer.feed_forward.linear_1.bias"] = input_model["albert.encoder.albert_layer_groups.0.albert_layers.0.ffn.bias"]
+output_model["encoder.transformer.feed_forward.linear_2.weight"] = input_model["albert.encoder.albert_layer_groups.0.albert_layers.0.ffn_output.weight"]
+output_model["encoder.transformer.feed_forward.linear_2.bias"] = input_model["albert.encoder.albert_layer_groups.0.albert_layers.0.ffn_output.bias"]
+
+output_model["target.sop_linear_1.weight"] = input_model["albert.pooler.weight"]
+output_model["target.sop_linear_1.bias"] = input_model["albert.pooler.bias"]
+output_model["target.sop_linear_2.weight"] = input_model["sop_classifier.classifier.weight"]
+output_model["target.sop_linear_2.bias"] = input_model["sop_classifier.classifier.bias"]
+output_model["target.mlm_linear_1.weight"] = input_model["predictions.dense.weight"]
+output_model["target.mlm_linear_1.bias"] = input_model["predictions.dense.bias"]
+output_model["target.mlm_linear_2.weight"] = input_model["predictions.decoder.weight"]
+output_model["target.mlm_linear_2.bias"] = input_model["predictions.bias"]
+output_model["target.layer_norm.gamma"] = input_model["predictions.LayerNorm.weight"]
+output_model["target.layer_norm.beta"] = input_model["predictions.LayerNorm.bias"]
+
+torch.save(output_model, args.output_model_path)
---
+++ scripts/convert_albert_from_uer_to_huggingface.py
@@ -0,0 +1,55 @@
+import torch
+import argparse
+import collections
+
+
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("--input_model_path", type=str, default="pytorch_model.bin",
+                    help=".")
+parser.add_argument("--output_model_path", type=str, default="huggingface_model.bin",
+                    help=".")
+
+args = parser.parse_args()
+
+input_model = torch.load(args.input_model_path)
+
+output_model = collections.OrderedDict()
+
+output_model["albert.embeddings.word_embeddings.weight"] = input_model["embedding.word_embedding.weight"]
+output_model["albert.embeddings.position_embeddings.weight"] = input_model["embedding.position_embedding.weight"]
+output_model["albert.embeddings.token_type_embeddings.weight"] = input_model["embedding.segment_embedding.weight"][1:, :]
+output_model["albert.embeddings.LayerNorm.weight"] = input_model["embedding.layer_norm.gamma"]
+output_model["albert.embeddings.LayerNorm.bias"] = input_model["embedding.layer_norm.beta"]
+
+output_model["albert.encoder.embedding_hidden_mapping_in.weight"] = input_model["encoder.linear.weight"]
+output_model["albert.encoder.embedding_hidden_mapping_in.bias"] = input_model["encoder.linear.bias"]
+output_model["albert.encoder.albert_layer_groups.0.albert_layers.0.full_layer_layer_norm.weight"] = input_model["encoder.transformer.layer_norm_2.gamma"]
+output_model["albert.encoder.albert_layer_groups.0.albert_layers.0.full_layer_layer_norm.bias"] = input_model["encoder.transformer.layer_norm_2.beta"]
+output_model["albert.encoder.albert_layer_groups.0.albert_layers.0.attention.query.weight"] = input_model["encoder.transformer.self_attn.linear_layers.0.weight"]
+output_model["albert.encoder.albert_layer_groups.0.albert_layers.0.attention.query.bias"] = input_model["encoder.transformer.self_attn.linear_layers.0.bias"]
+output_model["albert.encoder.albert_layer_groups.0.albert_layers.0.attention.key.weight"] = input_model["encoder.transformer.self_attn.linear_layers.1.weight"]
+output_model["albert.encoder.albert_layer_groups.0.albert_layers.0.attention.key.bias"] = input_model["encoder.transformer.self_attn.linear_layers.1.bias"]
+output_model["albert.encoder.albert_layer_groups.0.albert_layers.0.attention.value.weight"] = input_model["encoder.transformer.self_attn.linear_layers.2.weight"]
+output_model["albert.encoder.albert_layer_groups.0.albert_layers.0.attention.value.bias"] = input_model["encoder.transformer.self_attn.linear_layers.2.bias"]
+output_model["albert.encoder.albert_layer_groups.0.albert_layers.0.attention.dense.weight"] = input_model["encoder.transformer.self_attn.final_linear.weight"]
+output_model["albert.encoder.albert_layer_groups.0.albert_layers.0.attention.dense.bias"] = input_model["encoder.transformer.self_attn.final_linear.bias"]
+output_model["albert.encoder.albert_layer_groups.0.albert_layers.0.attention.LayerNorm.weight"] = input_model["encoder.transformer.layer_norm_1.gamma"]
+output_model["albert.encoder.albert_layer_groups.0.albert_layers.0.attention.LayerNorm.bias"] = input_model["encoder.transformer.layer_norm_1.beta"]
+output_model["albert.encoder.albert_layer_groups.0.albert_layers.0.ffn.weight"] = input_model["encoder.transformer.feed_forward.linear_1.weight"]
+output_model["albert.encoder.albert_layer_groups.0.albert_layers.0.ffn.bias"] = input_model["encoder.transformer.feed_forward.linear_1.bias"]
+output_model["albert.encoder.albert_layer_groups.0.albert_layers.0.ffn_output.weight"] = input_model["encoder.transformer.feed_forward.linear_2.weight"]
+output_model["albert.encoder.albert_layer_groups.0.albert_layers.0.ffn_output.bias"] = input_model["encoder.transformer.feed_forward.linear_2.bias"]
+
+output_model["albert.pooler.weight"] = input_model["target.sop_linear_1.weight"]
+output_model["albert.pooler.bias"] = input_model["target.sop_linear_1.bias"]
+output_model["sop_classifier.classifier.weight"] = input_model["target.sop_linear_2.weight"]
+output_model["sop_classifier.classifier.bias"] = input_model["target.sop_linear_2.bias"]
+output_model["predictions.dense.weight"] = input_model["target.mlm_linear_1.weight"]
+output_model["predictions.dense.bias"] = input_model["target.mlm_linear_1.bias"]
+output_model["predictions.LayerNorm.weight"] = input_model["target.layer_norm.gamma"]
+output_model["predictions.LayerNorm.bias"] = input_model["target.layer_norm.beta"]
+output_model["predictions.decoder.weight"] = input_model["target.mlm_linear_2.weight"]
+output_model["predictions.decoder.bias"] = input_model["target.mlm_linear_2.bias"]
+output_model["predictions.bias"] = input_model["target.mlm_linear_2.bias"]
+
+torch.save(output_model, args.output_model_path)
