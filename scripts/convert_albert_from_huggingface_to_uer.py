2435441795edd6ee9ce3d35ef53fb395bbf369f3
hhou435 <435405393@qq.com>
2021-05-21 10:44:46 +0800

    Update albert convert scripts

--- scripts/convert_albert_from_huggingface_to_uer.py
+++ scripts/convert_albert_from_huggingface_to_uer.py
@@ -11,13 +11,13 @@ parser.add_argument("--output_model_path", type=str, default="pytorch_model.bin"
 
 args = parser.parse_args()
 
-input_model = torch.load(args.input_model_path)
+input_model = torch.load(args.input_model_path, map_location="cpu")
 
 output_model = collections.OrderedDict()
 
 output_model["embedding.word_embedding.weight"] = input_model["albert.embeddings.word_embeddings.weight"]
 output_model["embedding.position_embedding.weight"] = input_model["albert.embeddings.position_embeddings.weight"]
-output_model["embedding.segment_embedding.weight"] = input_model["albert.embeddings.token_type_embeddings.weight"][1:, :]
+output_model["embedding.segment_embedding.weight"] = torch.cat((torch.Tensor([[0]*input_model["albert.embeddings.token_type_embeddings.weight"].size()[1]]), input_model["albert.embeddings.token_type_embeddings.weight"]), dim=0)
 output_model["embedding.layer_norm.gamma"] = input_model["albert.embeddings.LayerNorm.weight"]
 output_model["embedding.layer_norm.beta"] = input_model["albert.embeddings.LayerNorm.bias"]
 
