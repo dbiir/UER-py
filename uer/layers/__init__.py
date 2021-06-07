b76c787d585693972c185ad72ec759e4a7982601
P01son6415 <liyudong123@hotmail.com>
2021-06-07 17:36:06 +0800

    add PatchEmbedding

--- uer/layers/__init__.py
+++ uer/layers/__init__.py
@@ -2,10 +2,11 @@ from uer.layers.embeddings import WordEmbedding
 from uer.layers.embeddings import WordPosEmbedding
 from uer.layers.embeddings import WordPosSegEmbedding
 from uer.layers.embeddings import WordSinusoidalposEmbedding
+from uer.layers.embeddings import PatchEmbedding
 
 
-str2embedding = {"word": WordEmbedding, "word_pos": WordPosEmbedding, "word_pos_seg": WordPosSegEmbedding,
+str2embedding = {"patch": PatchEmbedding ,"word": WordEmbedding, "word_pos": WordPosEmbedding, "word_pos_seg": WordPosSegEmbedding,
                  "word_sinusoidalpos": WordSinusoidalposEmbedding}
 
-__all__ = ["WordEmbedding", "WordPosEmbedding", "WordPosSegEmbedding",
+__all__ = ["PatchEmbedding", "WordEmbedding", "WordPosEmbedding", "WordPosSegEmbedding",
            "WordSinusoidalposEmbedding", "str2embedding"]
