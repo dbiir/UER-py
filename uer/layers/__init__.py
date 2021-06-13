00db607d97fd87ce46f271a6844a4eb432931a9a
P01son6415 <liyudong123@hotmail.com>
2021-06-13 14:46:59 +0800

    add CLIP

--- uer/layers/__init__.py
+++ uer/layers/__init__.py
@@ -7,7 +7,7 @@ from uer.layers.embeddings import PatchPosEmbedding
 from uer.layers.embeddings import ViLEmbedding
 from uer.layers.embeddings import ClipEmbedding
 
-str2embedding = {"patch_pos": PatchPosEmbedding , "patch": PatchEmbedding , "clip": "ClipEmbedding", "vil": ViLEmbedding ,"word": WordEmbedding,
+str2embedding = {"patch_pos": PatchPosEmbedding , "patch": PatchEmbedding , "clip": ClipEmbedding, "vil": ViLEmbedding ,"word": WordEmbedding,
                  "word_pos": WordPosEmbedding, "word_pos_seg": WordPosSegEmbedding,"word_sinusoidalpos": WordSinusoidalposEmbedding}
 
 __all__ = ["PatchPosEmbedding", "PatchEmbedding", "ViLEmbedding", "ClipEmbedding", "WordEmbedding", "WordPosEmbedding",
