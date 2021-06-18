c4e3d6690235762a14a58f2d5aca46b6cb9f0c9b
P01son6415 <liyudong123@hotmail.com>
2021-06-18 15:26:28 +0800

    add run_classifier_image

---
+++ finetune/run_classifier_image.py
@@ -0,0 +1,182 @@
+"""
+This script provides an exmaple to wrap UER-py for classification.
+"""
+import sys
+import os
+import random
+import argparse
+import torch
+import torch.nn as nn
+import torchvision.datasets as dest
+from torchvision import transforms
+uer_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.append(uer_dir)
+
+from uer.layers import *
+from uer.encoders import *
+from uer.utils.vocab import Vocab
+from uer.utils.constants import *
+from uer.utils import *
+from uer.utils.optimizers import *
+from uer.utils.config import load_hyperparam
+from uer.utils.seed import set_seed
+from uer.model_saver import save_model
+from uer.opts import finetune_opts
+from finetune.run_classifier import *
+
+
+
+def count_labels_num(path):
+    labels_set, columns = set(), {}
+    with open(path, mode="r", encoding="utf-8") as f:
+        for line_id, line in enumerate(f):
+            if line_id == 0:
+                for i, column_name in enumerate(line.strip().split("\t")):
+                    columns[column_name] = i
+                continue
+            line = line.strip().split("\t")
+            label = int(line[columns["label"]])
+            labels_set.add(label)
+    return len(labels_set)
+
+str2dest = {"cifar10": dset.CIFAR10, "cifar100": dset.CIFAR100, "mnist": dset.MNIST}
+
+def read_dataset(args, path, train = True):
+    datasource = str2dest[args.dataset](path, train=train, transform=None, target_transform=None, download=args.download)
+    dataset = []
+    for i in range(len(datasource)):
+        src = transforms.ToTensor(datasource[i][0])
+        tgt = datasource[i][1]
+        seg = [1] * ((src.size()[1] // args.patch_size) * (src.size()[2] // args.patch_size) + 1)
+        dataset.append((src, tgt, seg))
+
+    return dataset
+
+
+def train_model(args, model, optimizer, scheduler, src_batch, tgt_batch, seg_batch, soft_tgt_batch=None):
+    model.zero_grad()
+
+    src_batch = src_batch.to(args.device)
+    tgt_batch = tgt_batch.to(args.device)
+    seg_batch = seg_batch.to(args.device)
+    if soft_tgt_batch is not None:
+        soft_tgt_batch = soft_tgt_batch.to(args.device)
+
+    loss, _ = model(src_batch, tgt_batch, seg_batch, soft_tgt_batch)
+    if torch.cuda.device_count() > 1:
+        loss = torch.mean(loss)
+
+    if args.fp16:
+        with args.amp.scale_loss(loss, optimizer) as scaled_loss:
+            scaled_loss.backward()
+    else:
+        loss.backward()
+
+    optimizer.step()
+    scheduler.step()
+
+    return loss
+
+
+def main():
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    finetune_opts(parser)
+
+    parser.add_argument("--pooling", choices=["mean", "max", "first", "last"], default="first",
+                        help="Pooling type.")
+
+    parser.add_argument("--dataset", choices=["mnist", "cifar10", "cifar100"], default="mnist",
+                        help="Image dataset type.")
+
+    parser.add_argument("--download", action='store_true',
+                        help="Download from internet and save on train path.")
+
+    parser.add_argument("--soft_targets", action='store_true',
+                        help="Train model with logits.")
+    parser.add_argument("--soft_alpha", type=float, default=0.5,
+                        help="Weight of the soft targets loss.")
+
+    args = parser.parse_args()
+
+    # Load the hyperparameters from the config file.
+    args = load_hyperparam(args)
+
+    set_seed(args.seed)
+
+    # Count the number of labels.
+    args.labels_num = count_labels_num(args.train_path)
+
+    # Build classification model.
+    model = Classifier(args)
+
+    # Load or initialize parameters.
+    load_or_initialize_parameters(args, model)
+
+    args.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = model.to(args.device)
+
+    # Training phase.
+    trainset = read_dataset(args, args.train_path)
+    random.shuffle(trainset)
+    instances_num = len(trainset)
+    batch_size = args.batch_size
+
+    src = torch.LongTensor([example[0] for example in trainset])
+    tgt = torch.LongTensor([example[1] for example in trainset])
+    seg = torch.LongTensor([example[2] for example in trainset])
+    if args.soft_targets:
+        soft_tgt = torch.FloatTensor([example[3] for example in trainset])
+    else:
+        soft_tgt = None
+
+    args.train_steps = int(instances_num * args.epochs_num / batch_size) + 1
+
+    print("Batch size: ", batch_size)
+    print("The number of training instances:", instances_num)
+
+    optimizer, scheduler = build_optimizer(args, model)
+
+    if args.fp16:
+        try:
+            from apex import amp
+        except ImportError:
+            raise ImportError("Please install apex from https://www.github.com/nvidia/apex to use fp16 training.")
+        model, optimizer = amp.initialize(model, optimizer, opt_level=args.fp16_opt_level)
+        args.amp = amp
+
+    if torch.cuda.device_count() > 1:
+        print("{} GPUs are available. Let's use them.".format(torch.cuda.device_count()))
+        model = torch.nn.DataParallel(model)
+    args.model = model
+
+    total_loss, result, best_result = 0.0, 0.0, 0.0
+
+    print("Start training.")
+
+    for epoch in range(1, args.epochs_num + 1):
+        model.train()
+        for i, (src_batch, tgt_batch, seg_batch, soft_tgt_batch) in enumerate(batch_loader(batch_size, src, tgt, seg, soft_tgt)):
+            loss = train_model(args, model, optimizer, scheduler, src_batch, tgt_batch, seg_batch, soft_tgt_batch)
+            total_loss += loss.item()
+            if (i + 1) % args.report_steps == 0:
+                print("Epoch id: {}, Training steps: {}, Avg loss: {:.3f}".format(epoch, i + 1, total_loss / args.report_steps))
+                total_loss = 0.0
+
+        result = evaluate(args, read_dataset(args, args.dev_path, train=False))
+        if result[0] > best_result:
+            best_result = result[0]
+            save_model(model, args.output_model_path)
+
+    # Evaluation phase.
+    if args.test_path is not None:
+        print("Test set evaluation.")
+        if torch.cuda.device_count() > 1:
+            model.module.load_state_dict(torch.load(args.output_model_path))
+        else:
+            model.load_state_dict(torch.load(args.output_model_path))
+        evaluate(args, read_dataset(args, args.test_path), True)
+
+
+if __name__ == "__main__":
+    main()
