8bfcec51ad2af9da0e5db1b6c7aad745b05a5675
zhezhaoa <1152543959@qq.com>
2021-06-27 20:24:26 +0800

    use Unix line endings uniformly

--- uer/encoders/transformer_encoder.py
+++ uer/encoders/transformer_encoder.py
@@ -95,10 +95,12 @@ class TransformerEncoder(nn.Module):
         for i in range(self.layers_num):
             if self.parameter_sharing:
                 hidden, prev_attn = self.transformer(hidden, mask, position_bias=position_bias,
-                                                     has_residual_attention=self.has_residual_attention, prev_attn=prev_attn)
+                                                     has_residual_attention=self.has_residual_attention,
+                                                     prev_attn=prev_attn)
             else:
                 hidden, prev_attn = self.transformer[i](hidden, mask, position_bias=position_bias,
-                                                        has_residual_attention=self.has_residual_attention, prev_attn=prev_attn)
+                                                        has_residual_attention=self.has_residual_attention,
+                                                        prev_attn=prev_attn)
 
         if self.layernorm_positioning == "pre":
             return self.layer_norm(hidden)
