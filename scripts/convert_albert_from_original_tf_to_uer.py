dab4409cd135b3f457a924d564360b9877344c55
chenghou <435405393@qq.com>
2021-07-27 09:41:31 +0800

    add albert convert scripts

---
+++ scripts/convert_albert_from_original_tf_to_uer.py
@@ -0,0 +1,116 @@
+import argparse
+import collections
+import torch
+import numpy as np
+import tensorflow as tf
+from tensorflow.python import pywrap_tensorflow
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    # Path options.
+    parser.add_argument("--input_model_path", type=str, default='models/albert_base_chinese.ckpt',
+                        help="Path of the input model.")
+    parser.add_argument("--output_model_path", type=str, default='uer.bin',
+                        help="Path of the output model.")
+
+    args = parser.parse_args()
+
+    # print("Converting TensorFlow checkpoint from {}".format(args.input_model_path))
+
+    # Load weights from TensorFlow checkpoint.
+#    init_vars = tf.train.list_variables(args.input_model_path)
+#    names = []
+#    arrays = []
+#    for name, shape in init_vars:
+        # print("Loading TF weight {} with shape {}".format(name, shape))
+#        array = tf.train.load_variable(args.input_model_path, name)
+#        names.append(name)
+#        arrays.append(array)
+
+    reader = pywrap_tensorflow.NewCheckpointReader(args.input_model_path)
+    var_to_shape_map = reader.get_variable_to_shape_map()
+
+    input_model = collections.OrderedDict()
+
+    tensors_to_transopse = (
+        "kernel",
+    )
+
+#    for key, array in zip(names, arrays):
+#        torch_tensor = torch.Tensor(array)
+    for key in var_to_shape_map:
+        torch_tensor = reader.get_tensor(key)
+        if any([x in key for x in tensors_to_transopse]):
+            torch_tensor = torch_tensor.T
+        if key == "bert/embeddings/token_type_embeddings":
+            #torch_tensor = torch.cat((torch.Tensor([[0] * torch_tensor.size()[1]]), torch_tensor), dim=0)
+            col_dim = torch_tensor.shape[1]
+            sess = tf.Session()
+            zeros_var = tf.Variable(tf.zeros([1, col_dim], dtype=tf.float32), name="zeros_var")
+            sess.run(zeros_var.initializer)
+            torch_tensor = sess.run(tf.concat([sess.run(zeros_var), torch_tensor], 0))
+        input_model[key] = torch.Tensor(torch_tensor)
+
+    output_model = collections.OrderedDict()
+
+    output_model["embedding.word_embedding.weight"] = input_model["bert/embeddings/word_embeddings"]
+    output_model["encoder.linear.weight"] = input_model["bert/encoder/embedding_hidden_mapping_in/kernel"]
+    output_model["encoder.linear.bias"] = input_model["bert/encoder/embedding_hidden_mapping_in/bias"]
+    output_model["embedding.position_embedding.weight"] = input_model["bert/embeddings/position_embeddings"][:512]
+    output_model["embedding.segment_embedding.weight"] = input_model["bert/embeddings/token_type_embeddings"]
+    output_model["embedding.layer_norm.gamma"] = input_model["bert/embeddings/LayerNorm/gamma"]
+    output_model["embedding.layer_norm.beta"] = input_model["bert/embeddings/LayerNorm/beta"]
+
+    output_model["encoder.transformer.self_attn.linear_layers.0.weight"] = input_model[
+        "bert/encoder/transformer/group_0/inner_group_0/attention_1/self/query/kernel"]
+    output_model["encoder.transformer.self_attn.linear_layers.0.bias"] = input_model[
+        "bert/encoder/transformer/group_0/inner_group_0/attention_1/self/query/bias"]
+    output_model["encoder.transformer.self_attn.linear_layers.1.weight"] = input_model[
+        "bert/encoder/transformer/group_0/inner_group_0/attention_1/self/key/kernel"]
+    output_model["encoder.transformer.self_attn.linear_layers.1.bias"] = input_model[
+        "bert/encoder/transformer/group_0/inner_group_0/attention_1/self/key/bias"]
+    output_model["encoder.transformer.self_attn.linear_layers.2.weight"] = input_model[
+        "bert/encoder/transformer/group_0/inner_group_0/attention_1/self/value/kernel"]
+    output_model["encoder.transformer.self_attn.linear_layers.2.bias"] = input_model[
+        "bert/encoder/transformer/group_0/inner_group_0/attention_1/self/value/bias"]
+    output_model["encoder.transformer.self_attn.final_linear.weight"] = input_model[
+        "bert/encoder/transformer/group_0/inner_group_0/attention_1/output/dense/kernel"]
+    output_model["encoder.transformer.self_attn.final_linear.bias"] = input_model[
+        "bert/encoder/transformer/group_0/inner_group_0/attention_1/output/dense/bias"]
+    output_model["encoder.transformer.layer_norm_1.gamma"] = input_model[
+        "bert/encoder/transformer/group_0/inner_group_0/LayerNorm/gamma"]
+    output_model["encoder.transformer.layer_norm_1.beta"] = input_model[
+        "bert/encoder/transformer/group_0/inner_group_0/LayerNorm/beta"]
+    output_model["encoder.transformer.feed_forward.linear_1.weight"] = input_model[
+        "bert/encoder/transformer/group_0/inner_group_0/ffn_1/intermediate/dense/kernel"]
+    output_model["encoder.transformer.feed_forward.linear_1.bias"] = input_model[
+        "bert/encoder/transformer/group_0/inner_group_0/ffn_1/intermediate/dense/bias"]
+    output_model["encoder.transformer.feed_forward.linear_2.weight"] = input_model[
+        "bert/encoder/transformer/group_0/inner_group_0/ffn_1/intermediate/output/dense/kernel"]
+    output_model["encoder.transformer.feed_forward.linear_2.bias"] = input_model[
+        "bert/encoder/transformer/group_0/inner_group_0/ffn_1/intermediate/output/dense/bias"]
+    output_model["encoder.transformer.layer_norm_2.gamma"] = input_model[
+        "bert/encoder/transformer/group_0/inner_group_0/LayerNorm_1/gamma"]
+    output_model["encoder.transformer.layer_norm_2.beta"] = input_model[
+        "bert/encoder/transformer/group_0/inner_group_0/LayerNorm_1/beta"]
+
+    output_model["target.sop_linear_1.weight"] = input_model["bert/pooler/dense/kernel"]
+    output_model["target.sop_linear_1.bias"] = input_model["bert/pooler/dense/bias"]
+    output_model["target.sop_linear_2.weight"] = input_model["cls/seq_relationship/output_weights"]
+    output_model["target.sop_linear_2.bias"] = input_model["cls/seq_relationship/output_bias"]
+    output_model["target.mlm_linear_1.weight"] = input_model["cls/predictions/transform/dense/kernel"]
+    output_model["target.mlm_linear_1.bias"] = input_model["cls/predictions/transform/dense/bias"]
+    output_model["target.layer_norm.gamma"] = input_model["cls/predictions/transform/LayerNorm/gamma"]
+    output_model["target.layer_norm.beta"] = input_model["cls/predictions/transform/LayerNorm/beta"]
+    output_model["target.mlm_linear_2.weight"] = input_model["bert/embeddings/word_embeddings"]
+    output_model["target.mlm_linear_2.bias"] = input_model["cls/predictions/output_bias"]
+
+    torch.save(output_model, args.output_model_path)
+
+    print("Conversion finished.")
+
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/convert_albert_from_uer_to_original_tf.py
@@ -0,0 +1,92 @@
+import os
+import sys
+import argparse
+import collections
+import numpy as np
+import tensorflow as tf
+import torch
+
+#tencentpretrain_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+#sys.path.insert(0, tencentpretrain_dir)
+
+#from scripts.convert_bert_from_original_tf_to_tencentpretrain import tensors_to_transopse
+
+tensors_to_transopse = (
+        "kernel",
+    )
+
+def assign_tf_var(tensor: np.ndarray, name: str):
+    tf_var = tf.get_variable(dtype=tensor.dtype, shape=tensor.shape, name=name)
+    tf.keras.backend.set_value(tf_var, tensor)
+    return tf_var
+
+
+def main():
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("--input_model_path", type=str, default="uer.bin",
+                        help=".")
+    parser.add_argument("--output_model_path", type=str, default="models/albert_base_chinese.ckpt",
+                        help=".")
+
+    args = parser.parse_args()
+
+    input_model = torch.load(args.input_model_path, map_location="cpu")
+
+    session = tf.Session()
+    tf.keras.backend.set_session(session)
+
+    output_model = collections.OrderedDict()
+
+    output_model["bert/embeddings/word_embeddings"] = input_model["embedding.word_embedding.weight"]
+    output_model["bert/encoder/embedding_hidden_mapping_in/kernel"] = input_model["encoder.linear.weight"]
+    output_model["bert/encoder/embedding_hidden_mapping_in/bias"] = input_model["encoder.linear.bias"]
+    output_model["bert/embeddings/position_embeddings"] = input_model["embedding.position_embedding.weight"][:512]
+    output_model["bert/embeddings/token_type_embeddings"] = input_model["embedding.segment_embedding.weight"]
+    output_model["bert/embeddings/LayerNorm/gamma"] = input_model["embedding.layer_norm.gamma"]
+    output_model["bert/embeddings/LayerNorm/beta"] = input_model["embedding.layer_norm.beta"]
+
+    output_model["bert/encoder/transformer/group_0/inner_group_0/attention_1/self/query/kernel"] = input_model["encoder.transformer.self_attn.linear_layers.0.weight"]
+    output_model["bert/encoder/transformer/group_0/inner_group_0/attention_1/self/query/bias"] = input_model["encoder.transformer.self_attn.linear_layers.0.bias"]
+    output_model["bert/encoder/transformer/group_0/inner_group_0/attention_1/self/key/kernel"] = input_model["encoder.transformer.self_attn.linear_layers.1.weight"]
+    output_model["bert/encoder/transformer/group_0/inner_group_0/attention_1/self/key/bias"] = input_model["encoder.transformer.self_attn.linear_layers.1.bias"]
+    output_model["bert/encoder/transformer/group_0/inner_group_0/attention_1/self/value/kernel"] = input_model["encoder.transformer.self_attn.linear_layers.2.weight"]
+    output_model["bert/encoder/transformer/group_0/inner_group_0/attention_1/self/value/bias"] = input_model["encoder.transformer.self_attn.linear_layers.2.bias"]
+    output_model["bert/encoder/transformer/group_0/inner_group_0/attention_1/output/dense/kernel"] = input_model["encoder.transformer.self_attn.final_linear.weight"]
+    output_model["bert/encoder/transformer/group_0/inner_group_0/attention_1/output/dense/bias"] = input_model["encoder.transformer.self_attn.final_linear.bias"]
+    output_model["bert/encoder/transformer/group_0/inner_group_0/LayerNorm/gamma"] = input_model["encoder.transformer.layer_norm_1.gamma"]
+    output_model["bert/encoder/transformer/group_0/inner_group_0/LayerNorm/beta"] = input_model["encoder.transformer.layer_norm_1.beta"]
+    output_model["bert/encoder/transformer/group_0/inner_group_0/ffn_1/intermediate/dense/kernel"] = input_model["encoder.transformer.feed_forward.linear_1.weight"]
+    output_model["bert/encoder/transformer/group_0/inner_group_0/ffn_1/intermediate/dense/bias"] = input_model["encoder.transformer.feed_forward.linear_1.bias"]
+    output_model["bert/encoder/transformer/group_0/inner_group_0/ffn_1/intermediate/output/dense/kernel"] = input_model["encoder.transformer.feed_forward.linear_2.weight"]
+    output_model["bert/encoder/transformer/group_0/inner_group_0/ffn_1/intermediate/output/dense/bias"] = input_model["encoder.transformer.feed_forward.linear_2.bias"]
+    output_model["bert/encoder/transformer/group_0/inner_group_0/LayerNorm_1/gamma"] = input_model["encoder.transformer.layer_norm_2.gamma"]
+    output_model["bert/encoder/transformer/group_0/inner_group_0/LayerNorm_1/beta"] = input_model["encoder.transformer.layer_norm_2.beta"]
+
+    output_model["bert/pooler/dense/kernel"] = input_model["target.sop_linear_1.weight"]
+    output_model["bert/pooler/dense/bias"] = input_model["target.sop_linear_1.bias"]
+    output_model["cls/seq_relationship/output_weights"] = input_model["target.sop_linear_2.weight"]
+    output_model["cls/seq_relationship/output_bias"] = input_model["target.sop_linear_2.bias"]
+    output_model["cls/predictions/transform/dense/kernel"] = input_model["target.mlm_linear_1.weight"]
+    output_model["cls/predictions/transform/dense/bias"] = input_model["target.mlm_linear_1.bias"]
+    output_model["cls/predictions/transform/LayerNorm/gamma"] = input_model["target.layer_norm.gamma"]
+    output_model["cls/predictions/transform/LayerNorm/beta"] = input_model["target.layer_norm.beta"]
+    output_model["bert/embeddings/word_embeddings"] = input_model["target.mlm_linear_2.weight"]
+    output_model["cls/predictions/output_bias"] = input_model["target.mlm_linear_2.bias"]
+
+    tf_vars = []
+
+    for k, v in output_model.items():
+        tf_name = k
+        torch_tensor = v.cpu().numpy()
+        if any([x in k for x in tensors_to_transopse]):
+            torch_tensor = torch_tensor.T
+        tf_tensor = assign_tf_var(tensor=torch_tensor, name=tf_name)
+        tf_vars.append(tf_tensor)
+        print("{0}{1}initialized".format(tf_name, " " * (60 - len(tf_name))))
+
+    saver = tf.train.Saver(tf_vars)
+    saver.save(session, args.output_model_path)
+
+
+if __name__ == "__main__":
+    main()
