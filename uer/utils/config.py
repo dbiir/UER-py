0dcb93d6e70504e8e1b7ab21455d856048c31b50
Cheng hou <59219579+hhou435@users.noreply.github.com>
2021-08-02 16:34:40 +0800

    add support for Deepspeed https://github.com/microsoft/DeepSpeed (#187)

--- uer/utils/config.py
+++ uer/utils/config.py
@@ -6,6 +6,10 @@ def load_hyperparam(args):
     with open(args.config_path, mode="r", encoding="utf-8") as f:
         param = json.load(f)
 
+    if "deepspeed" in args.__dict__:
+        with open(args.deepspeed_config, mode="r", encoding="utf-8") as f:
+            args.deepspeed_config_param = json.load(f)
+
     args_dict = vars(args)
     args_dict.update(param)
     args = Namespace(**args_dict)
