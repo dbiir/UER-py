3a2eac2803ce78fdf628361d8421d33e3eff087f
hhou435 <435405393@qq.com>
2021-08-23 15:11:51 +0800

    Update parameter name

--- uer/utils/config.py
+++ uer/utils/config.py
@@ -3,23 +3,23 @@ import sys
 from argparse import Namespace
 
 
-def load_hyperparam(args):
+def load_hyperparam(default_args):
     """
     Load arguments form argparse and config file
     Priority: default options < config file < command line args
     """
-    with open(args.config_path, mode="r", encoding="utf-8") as f:
-        param = json.load(f)
+    with open(default_args.config_path, mode="r", encoding="utf-8") as f:
+        config_args_dict = json.load(f)
 
-    if "deepspeed" in args.__dict__:
-        with open(args.deepspeed_config, mode="r", encoding="utf-8") as f:
-            args.deepspeed_config_param = json.load(f)
+    if "deepspeed" in default_args.__dict__:
+        with open(default_args.deepspeed_config, mode="r", encoding="utf-8") as f:
+            default_args.deepspeed_config_param = json.load(f)
 
-    args_dict = vars(args)
+    default_args_dict = vars(default_args)
 
-    input_args = {k: args_dict[k] for k in [a[2:] for a in sys.argv if (a[:2] == "--" and "local_rank" not in a)]}
-    args_dict.update(param)
-    args_dict.update(input_args)
-    args = Namespace(**args_dict)
+    command_line_args_dict = {k: default_args_dict[k] for k in [a[2:] for a in sys.argv if (a[:2] == "--" and "local_rank" not in a)]}
+    default_args_dict.update(config_args_dict)
+    default_args_dict.update(command_line_args_dict)
+    args = Namespace(**default_args_dict)
 
     return args
