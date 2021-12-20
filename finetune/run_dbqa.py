729d91433f09ef7d7ba156fa8486d49b62c10983
WenH <80993860+Eric8932@users.noreply.github.com>
2021-12-20 19:16:22 +0800

    shuffle the dataset in every epoch

--- finetune/run_dbqa.py
+++ finetune/run_dbqa.py
@@ -165,13 +165,8 @@ def main():
 
     # Training phase.
     trainset = read_dataset(args, args.train_path)
-    random.shuffle(trainset)
     instances_num = len(trainset)
     batch_size = args.batch_size
-
-    src = torch.LongTensor([example[0] for example in trainset])
-    tgt = torch.LongTensor([example[1] for example in trainset])
-    seg = torch.LongTensor([example[2] for example in trainset])
 
     args.train_steps = int(instances_num * args.epochs_num / batch_size) + 1
 
@@ -201,6 +196,11 @@ def main():
     print("Start training.")
 
     for epoch in range(1, args.epochs_num + 1):
+        random.shuffle(trainset)
+        src = torch.LongTensor([example[0] for example in trainset])
+        tgt = torch.LongTensor([example[1] for example in trainset])
+        seg = torch.LongTensor([example[2] for example in trainset])
+    
         model.train()
         for i, (src_batch, tgt_batch, seg_batch, _) in enumerate(batch_loader(batch_size, src, tgt, seg)):
             loss = train_model(args, model, optimizer, scheduler, src_batch, tgt_batch, seg_batch)
