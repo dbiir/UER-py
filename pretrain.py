e584fbf899387b96c9dcfb5c3601d078bf79e5d2
Janfliegt <49253507+Janfliegt@users.noreply.github.com>
2022-01-19 18:46:11 +0800

    fix bug when use multi-gpus in pretraining

--- pretrain.py
+++ pretrain.py
@@ -1,10 +1,8 @@
 import argparse
 import torch
-import uer.trainer as trainer
-from uer.utils.config import load_hyperparam
-from uer.utils.logging import init_logger
-
-from uer.opts import *
+import tencentpretrain.trainer as trainer
+from tencentpretrain.utils.config import load_hyperparam
+from tencentpretrain.opts import *
 
 
 def main():
@@ -73,7 +71,7 @@ def main():
     # GPU options.
     parser.add_argument("--world_size", type=int, default=1, help="Total number of processes (GPUs) for training.")
     parser.add_argument("--gpu_ranks", default=[], nargs='+', type=int, help="List of ranks of each process."
-                                                                             " Each process has a unique integer rank whose value is in the interval [0, world_size), and runs in a single GPU.")
+                        " Each process has a unique integer rank whose value is in the interval [0, world_size), and runs in a single GPU.")
     parser.add_argument("--master_ip", default="tcp://localhost:12345", type=str, help="IP-Port of master for training.")
     parser.add_argument("--backend", choices=["nccl", "gloo"], default="nccl", type=str, help="Distributed backend.")
 
@@ -92,9 +90,6 @@ def main():
     if args.config_path:
         args = load_hyperparam(args)
 
-    # Get logger
-    args.logger = init_logger(args)
-
     ranks_num = len(args.gpu_ranks)
 
     if args.deepspeed:
@@ -110,7 +105,7 @@ def main():
             assert ranks_num <= torch.cuda.device_count(), "Started processes exceeds the available GPUs."
             args.dist_train = True
             args.ranks_num = ranks_num
-            args.logger.info("Using distributed mode for training.")
+            print("Using distributed mode for training.")
         elif args.world_size == 1 and ranks_num == 1:
             # Single GPU mode.
             assert torch.cuda.is_available(), "No available GPUs."
@@ -118,13 +113,13 @@ def main():
             assert args.gpu_id < torch.cuda.device_count(), "Invalid specified GPU device."
             args.dist_train = False
             args.single_gpu = True
-            args.logger.info("Using GPU %d for training." % args.gpu_id)
+            print("Using GPU %d for training." % args.gpu_id)
         else:
             # CPU mode.
             assert ranks_num == 0, "GPUs are specified, please check the arguments."
             args.dist_train = False
             args.single_gpu = False
-            args.logger.info("Using CPU mode for training.")
+            print("Using CPU mode for training.")
 
     trainer.train_and_validate(args)
 
