32b10ddfd8e5926986df97709c232b102c14466f
WenH <80993860+Eric8932@users.noreply.github.com>
2023-08-17 09:44:53 +0800

    Update model.py
    If tie_weights and having lm_target, the lm_target should be consistent with tgt_embedding first.

--- uer/models/model.py
+++ uer/models/model.py
@@ -21,10 +21,10 @@ class Model(nn.Module):
 
         if "mlm" in args.target and args.tie_weights:
             self.target.mlm.linear_2.weight = self.embedding.word.embedding.weight
+        elif "lm" in args.target and args.tie_weights and self.tgt_embedding is not None and "word" in self.tgt_embedding.embedding_name_list:
+            self.target.lm.output_layer.weight = self.tgt_embedding.word.embedding.weight
         elif "lm" in args.target and args.tie_weights and "word" in self.embedding.embedding_name_list:
             self.target.lm.output_layer.weight = self.embedding.word.embedding.weight
-        elif "lm" in args.target and args.tie_weights and "word" in self.tgt_embedding.embedding_name_list:
-            self.target.lm.output_layer.weight = self.tgt_embedding.word.embedding.weight
             
         if self.decoder is not None and args.share_embedding:
             self.tgt_embedding.word.embedding.weight = self.embedding.word.embedding.weight
